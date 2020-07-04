d73a031d34417503c8f8ce8ba0d78ff1612bf28e
elasmojs <66966290+elasmojs@users.noreply.github.com>
2020-07-04 12:10:29 +0530

    Adding script support
    Processed incoming request and passed to script as 'request' object

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ use std::clone::Clone;
 use std::fs::{metadata};
 use std::io::Error as IoError;
 use std::{net::SocketAddr, path::Path};
+use std::collections::HashMap;
+use std::str;
 
 use futures_util::future;
+use futures_util::stream::TryStreamExt;
 
-use hyper::{Body, Request, Response, Server, StatusCode};
+use hyper::{Body, Request, Response, Server, StatusCode, HeaderMap};
 use hyper::service::{make_service_fn, service_fn};
 use hyper_staticfile::Static;
 use hyper::server::conn::AddrStream;
@@ -26,11 +29,40 @@ const ADMIN_ROUTE:&str = "/_rooster";
 const SCRIPT_EXTN:&str = ".rhai";
 const F_SLASH:&str = "/";
 const DOT:&str = ".";
+const CONTENT_TYPE:&str = "content-type";
+const MULTI_PART_CONTENT_TYPE:&str = "multipart/form-data";
+
+pub struct RequestData{
+    pub method: String,
+    pub path: String,
+    pub query: String,
+    pub headers: HashMap<String, String>,
+    pub body: String,
+    pub is_multipart: bool
+}
+
+impl RequestData{
+    fn new(method: String, path: String, query: String, headers:HashMap<String, String>, body: String, is_multipart:bool) -> RequestData{
+        return RequestData{
+            method: method,
+            path: path,
+            query: query,
+            headers: headers,
+            body: body,
+            is_multipart: is_multipart
+        };
+    }
+}
 
 
 async fn process_request(req: Request<Body>, static_:Static, props: Props) -> Result<Response<Body>, IoError> {
     
-    let req_path = req.uri().path();
+    let uri = req.uri().clone();
+    let req_path = uri.path().clone();
+    let query = match uri.query().clone(){
+        Some(q) => String::from(q),
+        None => String::from("")
+    };
     let last_slash_idx = _last_index_of(String::from(req_path), F_SLASH) as usize;
     let file_name = &req_path[last_slash_idx+1..];
     let not_file = _last_index_of(String::from(file_name), DOT) == -1;
@@ -82,15 +114,89 @@ async fn process_request(req: Request<Body>, static_:Static, props: Props) -> Re
             //process rooster admin
             return admin::process(req_path, props).await;
         },
-        _req_path if is_script =>{
-            //process script call
-            return scripts::process(req, props);
+        req_path if is_script =>{
+            //process script
+            let method = req.method().clone().to_string();
+            let headers = get_headers(req.headers().clone()).await;
+            let path = String::from(req_path);
+
+            let mut body = String::from("");
+            
+            let mut is_multipart = false;
+            let has_content_type = headers.contains_key(CONTENT_TYPE);
+            if has_content_type{
+                match headers.get(CONTENT_TYPE){
+                    Some(ctype) => {
+                        if ctype.contains(MULTI_PART_CONTENT_TYPE){
+                            is_multipart = true;
+                        }
+                    },
+                    None => is_multipart = false
+                }
+            }
+
+            if !is_multipart{
+                body = get_body_str(req).await;
+                let req_data = RequestData::new(
+                    method.clone(),
+                    path.clone(),
+                    query.clone(),
+                    headers.clone(),
+                    body.clone(),
+                    false
+                );
+                return scripts::process_normal(req_data, props).await;
+            }else{
+                let req_data = RequestData::new(
+                    method.clone(),
+                    path.clone(),
+                    query.clone(),
+                    headers.clone(),
+                    body.clone(),
+                    true
+                );
+                return scripts::process_multi_part(req_data, props, req).await;
+            } 
         },        
         _ => {
-            //Process static files
-            return static_.clone().serve(req).await;
+                return static_.clone().serve(req).await;
         }
     }
+}
+
+async fn get_headers(headers:HeaderMap) -> HashMap<String, String>{
+    let mut headers_map = HashMap::new();
+    for (key, value) in headers.iter() {
+        let value_str = match value.clone().to_str(){
+            Ok(v) => String::from(v),
+            Err(_e) => String::from("")
+        };
+        headers_map.insert(key.clone().to_string().to_lowercase(), value_str);
+    }
+    return headers_map;
+}
+
+async fn get_body_str(req:Request<Body>) -> String{
+    let body = req.into_body();
+    let body_data = body.try_fold(Vec::new(), |mut data, chunk| async move {
+        data.extend_from_slice(&chunk);
+        return Ok(data);
+    }).await;
+
+    let body_vec = body_data.map(|v| {
+        return v;
+    });
+
+    let body_str = match body_vec{
+        Ok(vec) => {
+            String::from(str::from_utf8(&vec).unwrap())
+        },
+        Err(e) => {
+            format!("Error = {}", e)
+        }
+    };
+
+    return body_str;
 }
 
 async fn shutdown_signal() {
--- src/scripts.rs
+++ src/scripts.rs
@@ -1,22 +1,31 @@
 use std::fs::File;
 use std::io::{Error as IoError, Read};
+use std::str;
+use std::collections::HashMap;
 
-use hyper::{Body, Request, Response, header::HeaderValue, StatusCode};
+use hyper::{Body, Request, Response, header::HeaderValue, StatusCode, };
 
 use rhai::{Engine, OptimizationLevel, Scope, Dynamic, Map};
 
 use crate::props::Props;
+use crate::RequestData;
 
 const SCRIPT_EXTN:&str = ".rhai";
 const SCRIPT_MAIN:&str = "main";
 const SERVER_ERROR:u16 = 500;
 
-pub fn process(req: Request<Body>, props: Props) -> Result<Response<Body>, IoError>{
+pub async fn process_multi_part(req_data:RequestData, props: Props, _req:Request<Body>) -> Result<Response<Body>, IoError>{
+    return process(req_data, props).await;
+}
+
+pub async fn process_normal(req_data:RequestData, props: Props) -> Result<Response<Body>, IoError>{
+    return process(req_data, props).await;
+}
+
+async fn process(req_data:RequestData, props:Props) -> Result<Response<Body>, IoError>{
     //TODO: process script
     
-    let req_path = req.uri().path();
-    
-    let mut script_file = match File::open(&(props.web_root.clone() + req_path + SCRIPT_EXTN)) {
+    let mut script_file = match File::open(&(props.web_root.clone() + &req_data.path.clone() + SCRIPT_EXTN)) {
         Err(_err) => {
             let mut response = Response::default();
             *response.status_mut() = StatusCode::NOT_FOUND;
@@ -41,47 +50,28 @@ pub fn process(req: Request<Body>, props: Props) -> Result<Response<Body>, IoErr
     engine.set_max_modules(1000);
     engine.set_max_map_size(1500);
     engine.set_max_array_size(1500);
-    engine.set_max_string_size(5000);  
-
+    engine.set_max_string_size(5000);
+    
+    
     let ast = engine.compile(&contents);
     if ast.is_err() {
         println!("{:?}", ast.unwrap_err());
         return get_server_error_response()
     }
 
-    /*
-    TODO 
-    define custom type to pass request details and other necessary properties
-    define script api functions
-    enable script api option auto response
-  
-    E.g. of custom type registration below
-    engine.register_type::<Props>();
-    engine.register_get("net_port", Props::get_net_port);
-    engine.register_get("web_root", Props::get_net_port);
-    engine.register_get("web_default", Props::get_net_port);
-    engine.register_get("remote_addr", Props::get_net_port);
-    */
+    let mut request_map = Map::new();
 
-    let mut header_keys = req.headers().keys();
-    let mut headers_map = Map::new();
-    for x in 0..header_keys.len() {
-        let key:String = match header_keys.nth(x){
-            Some(k) => k.to_string(),
-            None => String::from("")
-        };
-        let key_ = key.clone();
-        if key.len() > 0 {
-            let value:String = match req.headers().get(key){
-                Some(v) => String::from(v.to_str().unwrap()),
-                None => String::from("")
-            };
-            headers_map.insert(key_.clone(), Dynamic::from(value));
-        }
-    }
-
+    let headers_map = get_headers(req_data.headers);
+    
+    request_map.insert(String::from("method"), Dynamic::from(req_data.method.clone()));
+    request_map.insert(String::from("path"), Dynamic::from(req_data.path.clone()));
+    request_map.insert(String::from("query"), Dynamic::from(req_data.query.clone()));
+    request_map.insert(String::from("headers"), Dynamic::from(headers_map.clone()));
+    request_map.insert(String::from("body"), Dynamic::from(req_data.body.clone()));
+    request_map.insert(String::from("is_multipart"), Dynamic::from(req_data.is_multipart));
+    
     let mut scope = Scope::new();
-    scope.push("headers", headers_map);
+    scope.push("request", request_map);
     
     let result:Dynamic = match engine.call_fn(&mut scope, &(ast.unwrap()), SCRIPT_MAIN, () ){
         Ok(result) => {
@@ -103,10 +93,18 @@ pub fn process(req: Request<Body>, props: Props) -> Result<Response<Body>, IoErr
             return get_server_error_response()
         }
     };
-
+    
     let mut response = Response::new(Body::from(result_str));
     response.headers_mut().insert("Cache-Control", HeaderValue::from_static("no-cache"));
     return Ok(response);
+}
+
+fn get_headers(headers:HashMap<String, String>) -> Map{
+    let mut headers_map = Map::new();
+    for (key, value) in headers{
+        headers_map.insert(key.clone(), Dynamic::from(value));
+    }
+    return headers_map;
 }
 
 fn get_server_error_response() -> Result<Response<Body>, IoError>{
